eeabf9f4756e506eb42b887c88daaed8be2930f3
dixit palak <palak.d.dixit@gmail.com>
2018-06-06 09:54:20 +0530

    Initial Commit

--- app/src/main/java/com/knoxpo/palak/geoquiz/QuizActivity.java
+++ app/src/main/java/com/knoxpo/palak/geoquiz/QuizActivity.java
@@ -35,7 +35,7 @@ public class QuizActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
-        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+       // mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
